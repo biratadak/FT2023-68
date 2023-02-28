1ad7da10328e4f11ab9cfb8d023e80751808d8c5
VEER <birat.adak@gmail.com>
2023-02-28 11:56:51 +0530

    changed file structure

---
+++ server/ajax/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>AJAX</title>
+</head>
+<body>
+    <div class="loader">
+    </div>
+    <div class="text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem maiores nemo minima totam error exercitationem ipsam ullam veritatis repellendus aliquam dolores voluptatem aperiam, non ut quae, magni cumque veniam fugit odio est ea. Architecto quis earum, ex ipsum maxime commodi ad eos laboriosam, pariatur animi inventore dolor quia assumenda tempora?
+    </div>
+    <button class="btn">show</button>
+    <div class="hidden">
+        <h1>it is hidden text</h1>
+    </div>
+    
+</body>
+</html>
--- server/class/features.php
+++ server/class/features.php
@@ -1,5 +1,5 @@
 <?php
-require ('../vendor/autoload.php');
+require('../../vendor/autoload.php');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use GuzzleHttp\Client;
@@ -7,14 +7,68 @@ use GuzzleHttp\Client;
 //Getting secret credentials using dotenv
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
-
+/**
+ * Provides some usefull features for the checking, validating and upload files.
+ * 
+ * @method onlyAlpha().
+ *   Checks wether given str is only alphabet or not.
+ * 
+ * @method onlyDigit().
+ *   Checks wether given str is only digit or not.
+ * 
+ * @method validImage().
+ *   Checks wether given image is jpg/png and under 500kb.
+ * 
+ * @method splitMarks().
+ *   Splits marks and remove '|' and return array of marks.
+ * 
+ * @method validPhoneNo().
+ *   Checks if phone no is valid or not.
+ * 
+ * @method validMailId1().
+ *   Checks if Mail Id is valid or not using RegEx.
+ * 
+ * @method validMailBox().
+ *   Checks if Mail Id is valid or not using MailBoxLayer and Guzzle.
+ * 
+ * @method sendMail().
+ *   sends mail to given email Id.
+ * 
+ * @method getURL().
+ *   Get response body of given URL using GuzzleHTTP client request.
+ * 
+ * @property string $name
+ *  Store name off the user.
+ * 
+ * @property string $mailId
+ *  Store mailId of the user.
+ * 
+ * @property string $marks
+ *  Store marks of the user.
+ * 
+ * @property string $phoneNo
+ *  Store phoneNo of the user.
+ * 
+ * @property string $imagePath
+ *  Store path of profile-pic of the user.
+ * 
+ **/
 class features
 {
-    public $name, $mailId, $marks, $phoneNo, $imagePath;
-    
-    //String methods here 
-    
-    // Checks if a string only contains alphabets and whitespaces
+    public $name;
+    public $mailId;
+    public $marks;
+    public $phoneNo;
+    public $imagePath;
+
+    // String methods here 
+
+    /** 
+     * Checks if a string only contains alphabets and whitespaces
+     * 
+     * @param  $string
+     *   stores the string to varify. 
+     **/
     function onlyAlpha($string)
     {
         if (preg_match("/^[a-zA-Z-' ]*$/", $string)) {
@@ -23,7 +77,13 @@ class features
             return FALSE;
         }
     }
-    // Fucntion to check the string only has digits
+
+    /** 
+     * Fucntion to check the string only has digits
+     * 
+     * @param  $string
+     *   stores the string to varify. 
+     **/
     function onlyDigit($string)
     {
         if (preg_match("/^[1-9][0-9]{0,15}$/", $string))
@@ -33,6 +93,15 @@ class features
     }
 
     // Image methods here
+    /** 
+     *   Checks wether given image is jpg/png and under 500kb.
+     * 
+     * @param  $imageSize
+     *   Stores the size of the image. 
+     * 
+     * @param  $imageType
+     *   Stores the datatype of the image. 
+     **/
     function validImage($imageSize, $imageType)
     {
         if (($imageSize / 1000) <= 500 && ($imageType == 'image/jpg' || $imageType == 'image/png' || $imageType == 'image/jpeg')) {
@@ -49,9 +118,13 @@ class features
 
     }
 
-    // Marks methods here
-
-    // This section splits the $marks string and return array of different strings
+    /** 
+     * Splits the $marks string and return array of different strings
+     * 
+     * @param  $marks
+     *   Stores the marks field data of the user. 
+     * 
+     **/
     function splitMarks($marks)
     {
         $lines = array();
@@ -66,9 +139,13 @@ class features
         return $lines;
     }
 
-    // Phone No methods here
-
-    // Phone no validation
+    /**
+     * Checks if the given phone no starts with +91 and has exactly 10 numbers starting with 6-9 
+     * 
+     * @param  $phoneNo
+     *   Stores the Phone No of the user. 
+     * 
+     **/
     function validPhoneNo($phoneNo)
     {
         if (preg_match("/^[+][9][1][6-9][0-9]{9}$/", $phoneNo))
@@ -77,9 +154,13 @@ class features
             return FALSE;
     }
 
-    // E-Mail methods here
-
-    // MailId validation with Regex
+    /**
+     * Checks if Mail Id is valid or not using RegEx.
+     * 
+     * @param  $mailId
+     *  Stores the Mail Id of the user. 
+     * 
+     **/
     function validMailId1($mailId)
     {
         if (preg_match("/^[a-z-.]{1,20}[@][a-z]{1,10}[.][c][o][m]$/", $mailId))
@@ -88,11 +169,17 @@ class features
             return FALSE;
     }
 
-    // MailId validation with mailBoxLayer API.
+    /**
+     * Checks Mail Id validation with mailBoxLayer API.
+     * 
+     * @param  $mailId
+     *  Stores the Mail Id of the user. 
+     * 
+     **/
     function validMailBox($mailId)
     {
 
-    ////// API Calling Using cURL library.//////
+        ////// API Calling Using cURL library.//////
 
         // $curl = curl_init();
         // // Mailbox Layer API calling
@@ -117,7 +204,7 @@ class features
         // curl_close($curl);
 
 
-    // API Calling using HttpGuzzle.
+        // API Calling using HttpGuzzle.
         $client = new Client([
             //base uri of the site
             'base_uri' => 'https://api.apilayer.com/ ?email=',
@@ -157,31 +244,55 @@ class features
     }
 
     //Send Mails using PHP-Mailer
-    function sendMail($mailId,$subject="Subject",$body="no data found"){
+    /** 
+     * Send Mails using PHP-Mailer. 
+     * @param  $mailId
+     *   takes mailId as input field data of the user. 
+     * 
+     **/
+    function sendMail($mailId, $subject = "Subject", $body = "no data found")
+    {
         $mail = new PHPMailer(true);
 
         try {
-            $mail->SMTPDebug = 0;                                       
-            $mail->isSMTP();                                            
-            $mail->Host       = 'smtp.gmail.com';                    
-            $mail->SMTPAuth   = true;                             
-            $mail->Username   = $_ENV['SMTPMail'];                 
-            $mail->Password   = $_ENV['SMTPKey'];                        
-            $mail->SMTPSecure = 'tls';                              
-            $mail->Port       = 587;  
-          
-            $mail->setFrom($mailId, 'PHP Advance Assignment 2');           
+            $mail->SMTPDebug = 0;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = $_ENV['SMTPMail'];
+            $mail->Password = $_ENV['SMTPKey'];
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+
+            $mail->setFrom($mailId, 'PHP Advance Assignment 2');
             $mail->addAddress($mailId);
-               
-            $mail->isHTML(true);                                  
+
+            $mail->isHTML(true);
             $mail->Subject = $subject;
-            $mail->Body    = $body;
+            $mail->Body = $body;
             $mail->AltBody = 'Body in plain text for non-HTML mail clients';
             $mail->send();
             echo "Mail has been sent successfully!";
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
+    }
+
+    /** 
+     * Get response body from url using Guzzle.
+     * @param  $url
+     *   takes url as input and return response body. 
+     * 
+     **/
+    function getURL($url)
+    {
+        $client = new Client([
+            //base uri of the site
+            'base_uri' => $url,
+        ]);
+
+        $request = $client->request('GET');
+        return $request->getBody();
     }
 
 
